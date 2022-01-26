2683a134ad826d763d60b3d85c2da30c4fc82716
RamanSB <ramansb100@gmail.com>
2022-01-26 20:02:50 +0000

    Updating origin to match front-end host domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const serverPort : number = 6942
 const DB_NAME : string = "nothing";
-let middlewares: Array<any> = [bodyParser.json(), cors({ origin: "http://localhost:3000", credentials: true }), cookieParser()];
+let middlewares: Array<any> = [bodyParser.json(), cors({ origin: "https://upbeat-shirley-7b067b.netlify.app", credentials: true }), cookieParser()];
 let server : Server = new Server(serverPort, middlewares, [routes], "");
 server.connectToDatabase(process.env.ENV === 'development' ? process.env.DB_URL + DB_NAME : process.env.DB_URL);
 server.listen();
